77ae0ec3a45f75a471bbdbdaaf2ad2a60fc2ad21
Pádraic Brady <padraic@padraic-desktop.(none)>
2010-01-16 17:17:09 +0000

    Added simple item pagination to entry view

--- application/modules/zfplanet/controllers/IndexController.php
+++ application/modules/zfplanet/controllers/IndexController.php
@@ -3,14 +3,28 @@
 class Zfplanet_IndexController extends Zend_Controller_Action
 {
 
+    protected $_paginator = null;
+
+    public function init()
+    {
+        $this->_paginator = new Zend_Paginator(
+            Doctrine_Core::getTable('Zfplanet_Model_Entry')
+        );
+        $this->view->paginator = $this->_paginator;
+    }
+
     public function indexAction()
     {
-        $entries = Doctrine_Query::create()
-            ->from('Zfplanet_Model_Entry')
-            ->orderBy('publishedDate DESC')
-            ->limit(10)
-            ->execute();
-        $this->view->entries = $entries;
+        $this->_paginator->setCurrentPageNumber(1);
+    }
+    
+    public function pageAction()
+    {
+        $this->_helper->viewRenderer->setNoRender();
+        $this->_paginator->setCurrentPageNumber(
+            $this->_getParam('pageNumber')
+        );
+        $this->render('index');
     }
 
 }
