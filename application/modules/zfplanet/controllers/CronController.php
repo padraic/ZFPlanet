993b70a77b0323c4239dd1472f4584120f37d09b
Pádraic Brady <padraic@padraic-desktop.(none)>
2010-01-24 15:38:58 +0000

    When polling omit Pubsubhubbub subscribed feeds (they are updated in real time separately)

--- application/modules/zfplanet/controllers/CronController.php
+++ application/modules/zfplanet/controllers/CronController.php
@@ -10,16 +10,30 @@ class Zfplanet_CronController extends Zend_Controller_Action
         }
     }
 
+    /**
+     * Polls all registered feeds. The actual polling is performed by the
+     * retrieved Models, so all we do here is get those Models and setup
+     * Zend_Feed_Reader caching/conditional GET support if configured.
+     * Polling is NOT performed for feeds which have a confirmed Pubsubhubbub
+     * subscription active.
+     *
+     * @return void
+     */
     public function pollAction()
     {
         $feeds = Doctrine_Query::create()
-            ->from('Zfplanet_Model_Feed')
-            ->execute();
+            ->from('Zfplanet_Model_Feed f')
+            ->where(
+                'f.uri NOT IN (SELECT s.topic_url FROM Zfplanet_Model_Subscription s WHERE s.subscription_state = ?)',
+                Zend_Feed_Pubsubhubbub::SUBSCRIPTION_VERIFIED
+            )->execute();
         if (!$feeds) {
             return;
         }
         $chelper = $this->_helper->getHelper('Cache');
         if ($chelper->hasCache('feed')) {
+            // configured for refreshing cached content at maximum
+            // 1 week intervals by default (see cli.ini)
             Zend_Feed_Reader::setCache($chelper->getCache('feed'));
             Zend_Feed_Reader::useHttpConditionalGet();
         }
