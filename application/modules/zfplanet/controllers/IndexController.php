b6e45c4f37b704e27af2d3cfeafa131e869b2b39
Pádraic Brady <padraic@padraic-desktop.(none)>
2010-01-25 13:19:23 +0000

    Added static page caching for main HTML

--- application/modules/zfplanet/controllers/IndexController.php
+++ application/modules/zfplanet/controllers/IndexController.php
@@ -11,6 +11,7 @@ class Zfplanet_IndexController extends Zend_Controller_Action
             Doctrine_Core::getTable('Zfplanet_Model_Entry')
         );
         $this->view->paginator = $this->_paginator;
+        $this->_helper->cache(array('index', 'page'), array('all-entries'));
     }
 
     public function indexAction()
