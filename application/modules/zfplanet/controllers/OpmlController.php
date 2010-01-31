c1a2d190f81fba796cc9bbd73dfb03542ffcac4a
Pádraic Brady <padraic@padraic-desktop.(none)>
2010-01-31 14:36:52 +0000

    Added basic OPML support, linked from Feed links on sidepanel.

--- application/modules/zfplanet/controllers/OpmlController.php
+++ application/modules/zfplanet/controllers/OpmlController.php
@@ -13,12 +13,36 @@ class Zfplanet_OpmlController extends Zend_Controller_Action
     public function indexAction()
     {
         $dom = new DOMDocument('1.0', 'UTF-8');
+        $dom->formatOutput = true;
         $opml = $dom->createElement('opml');
         $opml->setAttribute('version', '2.0');
         $dom->appendChild($opml);
+        $body = $dom->createElement('body');
+        $blogs = $this->_getBlogs();
+        foreach ($blogs as $blog) {
+            $outline = $dom->createElement('outline');
+            $outline->setAttribute('text', $blog->title);
+            $outline->setAttribute('htmlUrl', $blog->Blog->uri);
+            $outline->setAttribute('xmlUrl', $blog->uri);
+            $body->appendChild($outline);
+        }
+        $opml->appendChild($body);
         $this->getResponse()
-            ->setHeader('Content-type', 'application/opml+xml; charset=UTF-8')
+            ->setHeader('Content-type', 'application/xml; charset=UTF-8')
             ->setBody($dom->saveXML());
+    }
+    
+    protected function _getBlogs($count = null)
+    {
+        $q = Doctrine_Query::create()
+            ->from('Zfplanet_Model_Feed')
+            ->groupBy('title')
+            ->orderBy('title ASC');
+        if ($count) {
+            $q->limit($count);  
+        }
+        $bloggers = $q->execute();
+        return $bloggers;
     }
 
 }
