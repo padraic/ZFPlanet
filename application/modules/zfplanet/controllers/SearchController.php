ba47b0ce79625a2ca798fffa4bef3860d1f4f136
Pádraic Brady <padraic@padraic-desktop.(none)>
2010-02-15 17:38:07 +0000

    Updating Search ops - need to watch file permissions on search index so the CLI can build it...

--- application/modules/zfplanet/controllers/SearchController.php
+++ application/modules/zfplanet/controllers/SearchController.php
@@ -9,18 +9,17 @@ class Zfplanet_SearchController extends Zend_Controller_Action
             $this->_redirect('/');
         }
         $options = $this->getInvokeArg('bootstrap')->getOptions();
-        //try {
+        try {
             $index = Zend_Search_Lucene::open($options['search']['indexPath']);
-        //} catch (Zend_Search_Lucene_Exception $e) {
-        //    $index = Zend_Search_Lucene::create($options['search']['indexPath']);
-        //}
-        //try {
-            $query = Zend_Search_Lucene_Search_QueryParser::parse($_POST['search'], 'UTF-8');
-            var_dump($query);
-            $hits = $index->find($query);
-        //} catch (Zend_Search_Lucene_Exception $e) {
-        //    $hits = array();
-        //}
+        } catch (Zend_Search_Lucene_Exception $e) {
+            $index = Zend_Search_Lucene::create($options['search']['indexPath']);
+        }
+        try {
+            $query = Zend_Search_Lucene_Search_QueryParser::parse($_POST['search'], 'utf-8');
+            $hits = $index->find($_POST['search']);
+        } catch (Zend_Search_Lucene_Exception $e) {
+            $hits = array();
+        }
         $this->view->results = $hits;
     }
 
