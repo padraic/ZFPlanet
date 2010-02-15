2a96a1324ddb8324d1109e3fcd70a5e04a226c9e
Pádraic Brady <padraic@padraic-desktop.(none)>
2010-02-15 13:11:59 +0000

    Added skeleton SearchController and matching view

---
+++ application/modules/zfplanet/controllers/SearchController.php
@@ -0,0 +1,26 @@
+<?php
+
+class Zfplanet_SearchController extends Zend_Controller_Action
+{
+
+    public function indexAction()
+    {
+        if (!isset($_POST['search'])) {
+            $this->_redirect('/');
+        }
+        $options = $this->getInvokeArg('bootstrap')->getOptions();
+        try {
+            $index = Zend_Search_Lucene::open($options['search']['indexPath']);
+        } catch (Zend_Search_Lucene_Exception $e) {
+            $index = Zend_Search_Lucene::create($options['search']['indexPath']);
+        }
+        try {
+            $query = Zend_Search_Lucene_Search_QueryParser::parse($_POST['search'], 'UTF-8');
+            $hits = $index->find($query);
+        } catch (Zend_Search_Lucene_Exception $e) {
+            $hits = array();
+        }
+        $this->view->results = $hits;
+    }
+
+}
