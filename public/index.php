cc81976dbc24c7f8a2261e8d8e5b43413f535551
Pádraic Brady <padraic@padraic-desktop.(none)>
2010-02-15 19:36:07 +0000

    Added configuration caching to index.php (not added for CLI)

--- public/index.php
+++ public/index.php
@@ -17,16 +17,63 @@ set_include_path(implode(PATH_SEPARATOR, array(
 /** Zend_Application */
 require_once 'Zend/Application.php';
 
+/** Zend_Cache */
+require_once 'Zend/Cache.php';
+
+/** Zend_Config_Ini */
+require_once 'Zend/Config/Ini.php';
+
+// Check for cached configuration, or create one before passage to Zend_App
+// This should speed up bootstrapping by some small margin
+$frontendOptions = array(
+    'name' => 'File',
+    'params' => array(
+        'lifetime' => null,
+        'automatic_cleaning_factor' => 0,
+        'automatic_serialization' => true,
+        'master_files' => array(
+            APPLICATION_PATH . '/configs/application.ini',
+            APPLICATION_PATH . '/configs/site.ini',
+            APPLICATION_PATH . '/configs/http.ini'
+        )
+    )
+);
+$backendOptions = array(
+    'name' => 'File',
+    'params' => array(
+        'cache_dir' => APPLICATION_PATH . '/../data/cache/config',
+    )
+);
+$configCache = Zend_Cache::factory(
+    $frontendOptions['name'],
+    $backendOptions['name'],
+    $frontendOptions['params'],
+    $backendOptions['params']
+);
+$finalConfig = null;
+if (!($finalConfig = $configCache->load('configuration'))) {
+    $configFiles = array(
+        APPLICATION_PATH . '/configs/application.ini',
+        APPLICATION_PATH . '/configs/http.ini',
+        APPLICATION_PATH . '/configs/site.ini'
+    );
+    $masterConfig = null;
+    foreach($configFiles as $file) {
+        $config = new Zend_Config_Ini($file, APPLICATION_ENV, array('allowModifications'=>true));
+        if (is_null($masterConfig)) {
+            $masterConfig = $config;
+        } else {
+            $masterConfig->merge($config);
+        }
+    }
+    $finalConfig = $masterConfig->toArray();
+    $configCache->save($finalConfig, 'configuration');
+}
+
 // Create application, bootstrap, and run
 $application = new Zend_Application(
     APPLICATION_ENV,
-    array(
-        'config' => array(
-            APPLICATION_PATH . '/configs/application.ini',
-            APPLICATION_PATH . '/configs/http.ini',
-            APPLICATION_PATH . '/configs/site.ini'
-        )
-    )
+    $finalConfig
 );
 $application->bootstrap()
             ->run();
